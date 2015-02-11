98c29dc9d310d697afe984652d3c986f82a0be2e
Leo Roos <leo.roos@daimler.com>
2015-02-11 15:42:14 -0800

    add find bounding box functionality

---
+++ lib/rosemary/bounding_box.rb
@@ -0,0 +1,58 @@
+require 'builder'
+
+module Rosemary
+  # OpenStreetMap Boundary Box.
+  #
+  class BoundingBox < Element
+
+    attr_accessor :nodes, :ways, :relations, :minlat, :minlon, :maxlat, :maxlon
+
+    # Create new Node object.
+    #
+    # If +id+ is +nil+ a new unique negative ID will be allocated.
+    def initialize(attrs = {})
+      attrs = attrs.dup.stringify_keys!
+      @minlat = attrs['minlat'].to_f rescue nil
+      @minlon = attrs['minlon'].to_f rescue nil
+      @maxlat = attrs['maxlat'].to_f rescue nil
+      @maxlat = attrs['maxlon'].to_f rescue nil
+
+      @nodes = []
+      @ways = []
+      @relations = []
+    end
+
+
+    def type
+      'BoundingBoxBox'
+    end
+
+    # List of attributes for a Node
+    def attribute_list
+      [:minlat, :minlon, :maxlat, :maxlon]
+    end
+
+    def to_xml(options = {})
+      xml = options[:builder] ||= Builder::XmlMarkup.new
+      xml.instruct! unless options[:skip_instruct]
+      xml.osm(:generator => "rosemary v#{Rosemary::VERSION}", :version => Rosemary::Api::API_VERSION) do
+        xml.node(attributes) do
+          tags.to_xml(:builder => xml, :skip_instruct => true)
+        end
+      end
+    end
+
+    def <=>(another_node)
+      raise 'not implemented'
+      parent_compare = super(another_node)
+      # don't bother to compare more stuff if parent comparison failed
+      return parent_compare unless parent_compare == 0
+
+      return -1 if self.send(:tags) != another_node.send(:tags)
+
+      0
+    end
+
+
+  end
+end
